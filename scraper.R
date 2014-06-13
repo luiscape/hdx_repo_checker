974b13ca14a92b78fad2087e154c802a33e04edb
Luis Capelo <luiscape@gmail.com>
2014-06-13 08:13:03 -0400

    experimenting with tryCatch

--- scraper.R
+++ scraper.R
@@ -152,7 +152,7 @@ getDatasets <- function() {
         
         # Transform to a list in final iteration.
         if (i == b) { 
-            dataset_title <- as.list(dataset_name)
+            dataset_title <- as.list(dataset_title)
             message('dataset_title: done.')
         }
     }
@@ -170,17 +170,32 @@ getDatasets <- function() {
     # Getting download_error
     for (i in 1:b) {
         
+        tryCatch(
+            download.file(download_url[i])
+        )
+        
     }
     
     # Data error
     for (i in 1:b) {
         
+        # If timeout error, try again.
+        if () {
+            i = i - 1
+        }
+        
+        # If any other error, go to next iteration.
+        if () {}
+        err_mess <- paste('There was an', x, 'error. Trying again')
+        message(err_mess)
+        finally 
     }
     
     # Other error
     for (i in 1:b) {
         
     }
+    
 datasets_list
 }
 
@@ -204,4 +219,72 @@ getData <- function(dataset = NULL) {
 }
 
 # Inverting rows and columns.
+
+
+
+
+###### Experimenting with tryCatch ###### 
+tryCatch(
+    message(test), 
+    error = function(e) test,
+    finally = message('No error.')
+    )
+
+
+### snippet from http://stackoverflow.com/questions/12193779/how-to-write-trycatch-in-r
+
+urls <- c(
+    "http://stat.ethz.ch/R-manual/R-devel/library/base/html/connections.html",
+    "http://en.wikipedia.org/wiki/Xz",
+    "xxxxx"
+)
+readUrl <- function(url) {
+    out <- tryCatch(
+{
+    # Just to highlight: if you want to use more than one 
+    # R expression in the "try" part then you'll have to 
+    # use curly brackets.
+    # 'tryCatch()' will return the last evaluated expression 
+    # in case the "try" part was completed successfully
+    
+    message("This is the 'try' part")
+    
+    readLines(con=url, warn=FALSE) 
+    # The return value of `readLines()` is the actual value 
+    # that will be returned in case there is no condition 
+    # (e.g. warning or error). 
+    # You don't need to state the return value via `return()` as code 
+    # in the "try" part is not wrapped insided a function (unlike that
+    # for the condition handlers for warnings and error below)
+},
+error=function(cond) {
+    message(paste("URL does not seem to exist:", url))
+    message("Here's the original error message:")
+    message(cond)
+    # Choose a return value in case of error
+    return(NA)
+},
+warning=function(cond) {
+    message(paste("URL caused a warning:", url))
+    message("Here's the original warning message:")
+    message(cond)
+    # Choose a return value in case of warning
+    return(NULL)
+},
+finally={
+    # NOTE:
+    # Here goes everything that should be executed at the end,
+    # regardless of success or error.
+    # If you want more than one expression to be executed, then you 
+    # need to wrap them in curly brackets ({...}); otherwise you could
+    # just have written 'finally=<expression>' 
+    message(paste("Processed URL:", url))
+    message("Some other message at the end")
+}
+    )    
+return(out)
+}
+
+
+
 
